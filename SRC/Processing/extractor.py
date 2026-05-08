e0c23d29d3f9f3414e874eda46113dc45914c540
minhthu2610 <minhthu2xx5@gmail.com>
2026-05-08 16:23:38 +0700

    complete pdf extraction and text cleaning

---
+++ SRC/Processing/extractor.py
@@ -0,0 +1,13 @@
+import PyPDF2
+def extract_text_from_pdf(file_path):
+    text = ""
+    try:
+        with open(file_path, "rb") as file:
+            reader = PyPDF2.PdfReader(file)
+            for page in reader.pages:
+                extracted = page.extract_text()
+                if extracted:
+                    text += extracted
+    except Exception as e:
+        print("PDF reading error:", e)
+    return text
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from SRC.Processing.extractor import extract_text_from_pdf
+from SRC.Processing.cleaner import clean_text
+text = extract_text_from_pdf("Data/Raw/cv.pdf")
 
+cleaned_text = clean_text(text)
 
+print(cleaned_text)
